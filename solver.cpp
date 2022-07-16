bb7edf2e06e2c2e1648bcac35c56a0b9e702facf
BruhRain <66817172+BruhRain@users.noreply.github.com>
2022-07-17 00:55:23 +0100

    i will finish tmrw maybe

--- solver.cpp
+++ solver.cpp
@@ -1,4 +1,6 @@
 #include "z3/include/z3++.h"
+#include "json.hpp"
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -7,17 +9,36 @@
 #include <format>
 
 std::vector <int> seq{};
+std::fstream config("config.json")
+std::fstream predFile("number.prediction")
+std::string configContents;
 
 
+
+void readConfig()
+{
+    // reads contents of config.json and stores it all in a variable
+
+    std::cout << "Getting config.json contents...\n";
+
+    if (config.is_open()) {
+        while (config.good()) {
+            config >> configContents;
+        }
+    }
+}
 
 int predict()
 {
+    // Placeholder for now so code can compile
+    int predicted;
     std::string notice = std::format();
+    return predicted
 }
 
 int main()
 {
-    std::string startingStr = std::format("Getting Contents of config.json");
-    std::string contents = "";
-    std::ifstream file("config.json");
+    // void readConfig();
+    readConfig();
+    return 0;
 }
