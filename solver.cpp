a8db8e3ba11aa563bf4959d79e2cfe85b5185a49
BruhRain <66817172+BruhRain@users.noreply.github.com>
2022-07-17 16:01:45 +0100

    additions to solver.cpp, ugly code but IDC

--- solver.cpp
+++ solver.cpp
@@ -8,17 +8,22 @@
 #include <fstream>
 #include <format>
 
+using json = nlohmann::json;
+
 std::vector <int> seq{};
 std::fstream config("config.json");
 std::fstream predFile("number.prediction");
 std::string configContents;
 
+json configJson = json::parse(configContents);
 
+std::string channelID = configJson.value("channelID", "oops");
+std::string botID = configJson.value("botID", "oops");
+int maxValue = configJson.value("max", "oops");
 
 void readConfig()
 {
     // reads contents of config.json and stores it all in a variable
-
     std::cout << "Getting config.json contents...\n";
 
     if (config.is_open()) {
@@ -26,13 +31,29 @@ void readConfig()
             config >> configContents;
         }
     }
+
+    std::string question;
+    std::cout << "Would you like to see the config.json contents? (y/n)\n";
+    std::cin >> question;
+    // because why not
+    if (question == "y") {
+        std::cout << configJson << "\n";
+    } else {
+        std::cout << "Skipping..\n";
+    }
 }
 
 int predict()
 {
     // Placeholder for now so code can compile
     int predicted;
-    std::string notice = std::format("");
+
+    std::string notice = std::format("Channel: {}\nBot: {}\nMax: {}", channelID, botID, maxValue);
+    std::cout << notice << "\n";
+
+    z3::context context;
+    z3::solver solver(context);
+
     return predicted;
 }
 
@@ -40,5 +61,8 @@ int main()
 {
     // void readConfig();
     readConfig();
+    // int predict();
+    predict();
+
     return 0;
 }
